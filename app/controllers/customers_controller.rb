7e90b578b63ed4c0e578012c29953e2820f134c2
Kyle Geske <stungeye@gmail.com>
2017-04-03 19:17:35 +0000

    Added the session visit count and the start of the remember-to-call feature.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,7 @@
 class CustomersController < ApplicationController
+  before_action :initialize_session
+  before_action :increment_visit_count
+
   def index
     @customers = Customer.all
   end
@@ -6,5 +9,23 @@ class CustomersController < ApplicationController
 
   def show
     @customer = Customer.find(params[:id])
+  end
+
+  def remember_to_call
+    id = params[:id].to_i
+    session[:to_call_list] << id unless session[:to_call_list].include?(id)
+    redirect_to :back
+  end
+
+  private
+
+  def initialize_session
+    session[:visit_count] ||= 0
+    session[:to_call_list] ||= []
+  end
+
+  def increment_visit_count
+    session[:visit_count] += 1
+    @visit_count = session[:visit_count]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  root to: 'customers#index'
-
-  resources :customers, only: :show
-
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'customers#index'
+
+  resources :customers, only: [:index, :show] do
+    member do
+      post :remember_to_call # POST /customers/:id/remember_to_call/
+    end
+  end
+
+  # The above member route is equivalent to manually defining:
+  # post 'customers/:id/remember_to_call/' => "customers#remember_to_call", as: 'remember_to_call_customer', id: /\d+/
 end
