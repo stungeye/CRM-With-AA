ba49b4fc0f3a4cc4ce96d159c9e0c4479ea1a1fc
Kyle Geske <stungeye@gmail.com>
2017-04-05 19:21:36 +0000

    Removed the visit counter.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,9 @@
 class CustomersController < ApplicationController
-  before_action :increment_visit_count, except: [:remember_to_call]
+  before_action :initialize_session
 
   def index
     @customers = Customer.all
   end
-  # Automatically load the app/views/customers/index.html.erb
 
   def show
     @customer = Customer.find(params[:id])
@@ -12,19 +11,24 @@ class CustomersController < ApplicationController
 
   def remember_to_call
     id = params[:id].to_i
-    session[:to_call_list] ||= []
     session[:to_call_list] << id unless session[:to_call_list].include?(id)
     redirect_back(fallback_location: root_path)
   end
 
   private
 
-  def increment_visit_count
-    session[:visit_count] = visit_count + 1
+  def initialize_session
+    session[:visit_count] ||= 0
+    session[:to_call_list] ||= []
   end
 
+  def customers_to_call
+    Customer.find(session[:to_call_list])
+  end
+  helper_method :customers_to_call
+
   def visit_count
-    session[:visit_count] || 0
+    session[:visit_count]
   end
   helper_method :visit_count
 end
