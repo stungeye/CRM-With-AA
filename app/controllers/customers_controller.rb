72958889f4f99b2752a505ba6466932aed796164
Kyle Geske <stungeye@gmail.com>
2017-04-05 19:04:54 +0000

    Refactoring the session count and remember to call feature.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :initialize_session
-  before_action :increment_visit_count
+  before_action :increment_visit_count, except: [:remember_to_call]
 
   def index
     @customers = Customer.all
@@ -13,19 +12,19 @@ class CustomersController < ApplicationController
 
   def remember_to_call
     id = params[:id].to_i
+    session[:to_call_list] ||= []
     session[:to_call_list] << id unless session[:to_call_list].include?(id)
     redirect_back(fallback_location: root_path)
   end
 
   private
 
-  def initialize_session
-    session[:visit_count] ||= 0
-    session[:to_call_list] ||= []
+  def increment_visit_count
+    session[:visit_count] = visit_count + 1
   end
 
-  def increment_visit_count
-    session[:visit_count] += 1
-    @visit_count = session[:visit_count]
+  def visit_count
+    session[:visit_count] || 0
   end
+  helper_method :visit_count
 end
